6d0c0db0c8df3a3e75174e8b5aa08654f6462854
VINICYUS CESAR CORREIA <vinicyus.correia@univille.br>
2026-09-15 20:29:43 -0300

    Criação e implementação Main (Criada e testado por Victor Schappo)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MaquinaDeLavar minhaMaquina = new MaquinaDeLavar();
+        int opcao = -1;
 
+        while (opcao != 0) {
+            System.out.println("Menu Mmáquina de lavar");
+            System.out.println("1) Ligar máquina");
+            System.out.println("2) Desligar máquina");
+            System.out.println("3) Abrir tampa");
+            System.out.println("4) Fechar tampa");
+            System.out.println("5) Iniciar lavagem");
+            System.out.println("6) Pausar lavagem");
+            System.out.println("7) Retomar lavagem");
+            System.out.println("8) Concluir lavagem");
+            System.out.println("9) Iniciar centrifugação");
+            System.out.println("10) Concluir centrifugação");
+            System.out.println("0) Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    minhaMaquina.ligar();
+                    break;
+                case 2:
+                    minhaMaquina.desligar();
+                    break;
+                case 3:
+                    minhaMaquina.abrirTampa();
+                    break;
+                case 4:
+                    minhaMaquina.fecharTampa();
+                    break;
+                case 5:
+                    minhaMaquina.iniciarLavagem();
+                    break;
+                case 6:
+                    minhaMaquina.pausarLavagem();
+                    break;
+                case 7:
+                    minhaMaquina.retomarLavagem();
+                    break;
+                case 8:
+                    minhaMaquina.concluirLavagem();
+                    break;
+                case 9:
+                    minhaMaquina.iniciarCentrifugacao();
+                    break;
+                case 10:
+                    minhaMaquina.concluirCentrifugacao();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+        sc.close();
     }
 }
